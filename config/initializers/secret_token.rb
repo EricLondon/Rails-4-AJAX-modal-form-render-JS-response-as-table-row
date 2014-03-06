7ef6cc861292f6e82e490bdce9660812163b9147
Eric London <git@ericlondon.com>
2014-03-05 20:34:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4ajaxrender::Application.config.secret_key_base = 'd047c70a03f81d7d95c08ee8e0593857d6e7ba682a928b2f11ee91d8b94c5d84e02a83a48b3e71db980edd6525dbb14a174e44791290cbd4da384b39c936d721'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4ajaxrender::Application.config.session_store :cookie_store, key: '_rails4ajaxrender_session'
