f1845ab065dbdc58e5d9bf65e189575dab08d455
Eric London <eric.london@arcadiasolutions.com>
2018-02-09 12:32:40 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140306013732) do
 
-  create_table "people", force: true do |t|
+  create_table "people", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at"
